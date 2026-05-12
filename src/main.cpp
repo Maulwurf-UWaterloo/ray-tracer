e2d7d556682c9818b918c4e92bcaae4da8871a30
Maulwurf <d269wu@uwaterloo.ca>
2026-05-12 18:06:58 -0400

    feat: add vec3 class and color utilities (RTIOW ch3)

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
-#include <fstream>
 
 int main() {
-    int width = 256;
-    int height = 256;
-    
-    std::ofstream out("output.ppm");
-    out << "P3\n" << width << " " << height << "\n255\n";
-    
-    for (int j = height - 1; j >= 0; --j) {
-        for (int i = 0; i < width; ++i) {
-            int r = (i * 255) / width;      // left to right: black to red
-            int g = (j * 255) / height;     // bottom to top: black to green
-            int b = 0;
-            out << r << " " << g << " " << b << "\n";
+    // Image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    
-    std::cout << "Wrote output.ppm\n";
-    return 0;
+
+    std::clog << "\rDone.                 \n";
 }
