6a7e3a1b26359d6136d3b40a458c7b497ec82691
Maulwurf <d269wu@uwaterloo.ca>
2026-05-14 22:46:10 -0400

    feat: add defocus blur / depth of field (RTIOW ch13)

--- src/camera.h
+++ src/camera.h
@@ -17,7 +17,9 @@ class camera {
     double vfov = 90;                     
     point3 lookfrom = point3(0,0,0);      
     point3 lookat   = point3(0,0,-1);     
-    vec3   vup      = vec3(0,1,0);        
+    vec3   vup      = vec3(0,1,0); 
+    double defocus_angle = 0;    
+    double focus_dist    = 10;   
 
     void render(const hittable& world) {
         initialize();
@@ -46,7 +48,9 @@ class camera {
     point3 pixel00_loc;
     vec3   pixel_delta_u;
     vec3   pixel_delta_v;
-    vec3   u, v, w; 
+    vec3   u, v, w;
+    vec3 defocus_disk_u;
+    vec3 defocus_disk_v;
     
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -56,7 +60,7 @@ class camera {
         
         center = lookfrom;
         
-        auto focal_length = (lookfrom - lookat).length();
+        auto focal_length = focus_dist;
         auto theta = degrees_to_radians(vfov);
         auto h = std::tan(theta/2);
         auto viewport_height = 2 * h * focal_length;
@@ -74,39 +78,44 @@ class camera {
         
         auto viewport_upper_left = center - (focal_length * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
-
+    
     ray get_ray(int i, int j) const {
-        // Construct a camera ray originating from the origin and directed at randomly sampled
-        // point around the pixel location i, j.
-
         auto offset = sample_square();
         auto pixel_sample = pixel00_loc
-                          + ((i + offset.x()) * pixel_delta_u)
-                          + ((j + offset.y()) * pixel_delta_v);
-
-        auto ray_origin = center;
-        auto ray_direction = pixel_sample - ray_origin;
-
-        return ray(ray_origin, ray_direction);
-    }
+                      + ((i + offset.x()) * pixel_delta_u)
+                      + ((j + offset.y()) * pixel_delta_v);
+                      
+                      auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+                      auto ray_direction = pixel_sample - ray_origin;
+                      
+                      return ray(ray_origin, ray_direction);
+                    }
 
     vec3 sample_square() const {
         // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
-color ray_color(const ray& r, int depth, const hittable& world) const {
-    if (depth <= 0)
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth <= 0)
         return color(0,0,0);
-
-    hit_record rec;
-
-    if (world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
+        
+        hit_record rec;
+        
+        if (world.hit(r, 0.001, infinity, rec)) {
+            ray scattered;
+            color attenuation;
         if (rec.mat->scatter(r, rec, attenuation, scattered))
             return attenuation * ray_color(scattered, depth-1, world);
-        return color(0,0,0);
+            return color(0,0,0);
+    }
+
+    point3 defocus_disk_sample() const {
+    auto p = random_in_unit_disk();
+    return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
--- src/vec3.h
+++ src/vec3.h
@@ -118,6 +118,14 @@ inline vec3 random_unit_vector() {
     }
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 inline bool near_zero(const vec3& v) {
     auto s = 1e-8;
     return (std::fabs(v.e[0]) < s) && (std::fabs(v.e[1]) < s) && (std::fabs(v.e[2]) < s);
